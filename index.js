3568f04268c0b0df6e5c40cfde7511e0639bedf0
Aseel Cortbawi <aseel.cortbawi@gmail.com>
2025-06-04 18:41:16 -0400

    second commit

--- index.js
+++ index.js
@@ -22,28 +22,51 @@ const getpuppies = async () => {
 getpuppies();
 
 // ///section4
-const getPuppiesDetails = async (puppy) => {
-  //   const response = await fetch(puppy);
-};
-
-//   return $puppiesList;
-// }
-// add an event listener for when the user clicks on the li
 
 // //section 3
 const renderpuppiesList = () => {
+  console.log(state.puppies);
+  const $app = document.querySelector("#app");
   const $puppiesList = document.createElement("ul");
   state.puppies.forEach((puppy) => {
     // this line i can see everything i need
-    console.log(puppy);
+    // console.log(puppy);
     const li = document.createElement(`li`);
-    li.addEventListener(`click`, async () => {});
+    li.addEventListener(`click`, async () => {
+      //   li.innerText = puppy.name; this line everytime i click on a name of a puppy , the pic disapear
+      console.log(puppy.name);
+    });
+
     li.innerText = puppy.name;
+    const img = document.createElement("img");
+    img.classList.add("imgclass");
+    img.src = puppy.imageUrl;
+    li.appendChild(img);
+    img.style.width = "10%";
+    img.style.height = "auto";
+    img.style.padding = "20px";
+    li.style.border = "1px dashed black";
+    li.style.listStyleType = "none";
+
+    li.addEventListener(`click`, async () => {
+      //   li.innerText = puppy.name; this line everytime i click on a name of a puppy , the pic disapear
+      //   console.log(puppy.name);
+    });
+    // click on the name get me player details
+    //     player details is :
+    //     {
+    //   "name": "Crumpet",
+    //   "breed": "American Staffordshire Terrier",
+    //   "status": "bench",
+    //   "imageUrl": "http://r.ddmcdn.com/w_1012/s_f/o_1/cx_0/cy_0/cw_1012/ch_1518/APL/uploads/2019/12/Crumpet-PBXVI.jpg",
+    //   "teamId": 456
+    // }
     // console.log(li);
     $puppiesList.appendChild(li);
+    $app.appendChild($puppiesList);
   });
-  console.log($puppiesList);
 
   $app.appendChild($puppiesList);
+  //   console.log($app);
   //   return $puppiesList;
 };
